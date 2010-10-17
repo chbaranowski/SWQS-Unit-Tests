47e0d1613c4383e62f4334eeb68b64b3086af074
tux2323 <developer@noname>
2010-10-17 23:44:14 +0200

    Add Tests to Branch Coverage Sample Test

--- test/swqs/coverage/BranchCoverageSumUtilTest.java
+++ test/swqs/coverage/BranchCoverageSumUtilTest.java
@@ -1,15 +1,25 @@
 package swqs.coverage;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class BranchCoverageSumUtilTest {
 
 	@Test
-	public void testSum() {
-		SumUtil.sum(new int[]{2, 1}, 1);
-		SumUtil.sum(new int[]{2, 1}, 0);
+	public void testSum_21() {
+		assertThat(SumUtil.sum(new int[]{2}, 1), is(3));
+	}
+	
+	@Test
+	public void testSum_20() throws Exception {
+		assertThat(SumUtil.sum(new int[]{2}, 0), is(2));
+	}
+	
+	@Test
+	public void testSum_22() throws Exception {
+		assertThat(SumUtil.sum(new int[]{2}, 2), is(2));
 	}
 
 }
