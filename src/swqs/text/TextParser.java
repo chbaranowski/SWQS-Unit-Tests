e449ad7696f02d5c369d270d0393a477cbbc63f2
tux2323 <developer@noname>
2010-10-24 15:21:50 +0200

    clean up implementation

--- src/swqs/text/TextParser.java
+++ src/swqs/text/TextParser.java
@@ -7,15 +7,18 @@ public class TextParser {
 
 	public Map<String, Integer> parseTextWordCount(String text) {
 		HashMap<String, Integer> result = new HashMap<String, Integer>();
-		if(text != null && !text.isEmpty())
+		if(text != null)
 		{
-			String[] words = text.split(" ");
-			for (String word : words) {
-				word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
-				if(result.containsKey(word))
-					result.put(word, result.get(word)+1);
-				else
-					result.put(word, 1);
+			if(!text.trim().isEmpty())
+			{
+				String[] words = text.split(" ");
+				for (String word : words) {
+					word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+					if(result.containsKey(word))
+						result.put(word, result.get(word)+1);
+					else
+						result.put(word, 1);
+				}
 			}
 		}
 		return result;
